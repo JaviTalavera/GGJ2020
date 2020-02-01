e5f8697977b9c4177bab0a75a1c78c0f882d3840
CinnamonLuu <lucia.ortuno22@gmail.com>
2020-02-01 02:24:49 +0100

    script piezas

---
+++ GGJ2020/Assets/Scripts/Piece.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Piece : MonoBehaviour
+{
+    public enum pieceType { leg_l, leg_r, arm_l, arm_r, head };
+    public Color[] colors = { Color.red, Color.green, Color.blue, Color.white, Color.black };
+
+    public Color[] _colors;
+    public int _numberConectors;
+    public pieceType _type;
+    
+    
+    Piece(int numberConectors)
+    {
+        //random colors
+        for(int i=0;i< _numberConectors; i++)
+        {
+            _colors[i] = colors[Random.Range(0,colors.Length)];
+        }
+
+        //random piece type
+        switch (Random.Range(0, 5))
+        {
+            case 0:
+                _type = pieceType.leg_l;
+                break;
+
+            case 1:
+                _type = pieceType.leg_r;
+                break;
+
+            case 2:
+                _type = pieceType.arm_l;
+                break;
+
+            case 3:
+                _type = pieceType.arm_r;
+                break;
+
+            case 4:
+                _type = pieceType.head;
+                break;
+        }
+    }
+
+
+    
+}
