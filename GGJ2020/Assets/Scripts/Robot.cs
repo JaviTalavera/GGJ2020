c6b2936ed7142658563a5e361584b1b4bb2a5014
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2020-02-01 02:45:04 +0100

    Prueba robot pieza

---
+++ GGJ2020/Assets/Scripts/Robot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField] public static int maxPieces = 4;
+    public int _numPieces;
+    public GameObject[] _pieces;
+
+    [SerializeField] public GameObject _piecePrefab;
+
+    private void Start()
+    {
+        _numPieces = Random.Range(1, maxPieces);
+        _pieces = new GameObject[_numPieces];
+
+        for(int i=0; i < _numPieces; i++)
+        {
+            _pieces[i] = new GameObject();
+            _pieces[i].AddComponent<Piece>();
+            Debug.Log("Belt instantiated");
+        }
+    }
+}
