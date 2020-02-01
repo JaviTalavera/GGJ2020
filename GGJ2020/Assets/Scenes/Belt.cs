1416b6ab18d2e201fdda4131e31499a7dda70919
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2020-02-01 02:25:55 +0100

    Lo que llevamos de nuestra parte xd

---
+++ GGJ2020/Assets/Scenes/Belt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Belt : MonoBehaviour
+{
+    public static float speed;
+    public GameObject[] spawners;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * speed;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Change pieces
+        // push de las piezas que tenemos
+        // pop de las nuevas piezas
+        //Spawn -> Setear la posicion de las piezas
+    }
+}
---
+++ GGJ2020/Assets/Scenes/Robot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    public static int maxPieces;
+    public int _numPieces;
+    public Belt[] _pieces;
+
+    private void Start()
+    {
+        _numPieces = Random.Range(1, maxPieces);
+        _pieces = new Belt[_numPieces];
+    }
+}
