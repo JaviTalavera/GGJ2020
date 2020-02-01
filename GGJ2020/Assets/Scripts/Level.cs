265e0f608ab783af2367ff84f7875aa7102c36c9
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2020-02-01 07:54:13 +0100

    Main classes almost prototypated
    There are some functions that must be implemented. Take a look at ToDoList.txt.
    
    The code is pretty gross, so it has been commented to make it easier to understand. Take it with calm ;)

--- GGJ2020/Assets/Scripts/Level.cs
+++ GGJ2020/Assets/Scripts/Level.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Level parameters
+    private int _NPieces = 10;
+    private int _NRobots = 3;
+
+    //Object containers
+    public Queue<GameObject> _pieces;
+    public Queue<GameObject> _robots;
+
+    //Object references to instantiate objects
+    public GameObject _robotPrefab;
+    public GameObject _piecePrefab;
+
+    // Start is called before the first frame update. The Awake is called even before Start.
+    void Awake()
     {
-        
+        //Initialize robots (will initialize it's own pieces)
+        for(int i = 0; i<_NRobots;i++)
+        {
+            GameObject r = Instantiate(_robotPrefab);
+            _robots.Enqueue(r);                                             //Enter the robot to the data structure.
+            foreach (GameObject piece in r.GetComponent<Robot>()._pieces)   
+            {
+                _pieces.Enqueue(piece);                                     //Add the robot's pieces to the data structure.
+            }
+        }
+
+        //Initialize aditional pieces to add complexity
+        for (int i=_pieces.Count; i<_NPieces; i++)
+        {
+            GameObject p = Instantiate(_piecePrefab);
+            p.GetComponent<Piece>().Initialize();
+            _pieces.Enqueue(p);
+        }
+
+        //Shuffle pieces
+
+        //Initialize the belts
+
     }
 
     // Update is called once per frame
--- GGJ2020/Assets/Scripts/Piece.cs
+++ GGJ2020/Assets/Scripts/Piece.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    //General Piece information
     public enum pieceType { leg_l, leg_r, arm_l, arm_r, head };
-    public Color[] colors = { Color.red, Color.green, Color.blue, Color.white, Color.black };
+    private Color[] colors = { Color.red, Color.green, Color.blue, Color.white, Color.black };
 
-    public Color[] _colors;
-    public int _numberConectors;
+    //Piece's info
     public pieceType _type;
-    
-    
-    Piece(int numberConectors)
-    {
-        //random colors
-        for(int i=0;i< _numberConectors; i++)
-        {
-            _colors[i] = colors[Random.Range(0,colors.Length)];
-        }
 
-        //random piece type
-        switch (Random.Range(0, 5))
+    //Conectors' info
+    private int _numberConectors;
+    private int _maxConectors = 3;
+    public Color[] _colors;
+    
+    //Conector reference to intantiate conector objects (the things that came out of the piece)
+    [SerializeField] public GameObject _conectorPrefab;
+
+    void Start()
+    {
+        //No action is required here.
+    }
+
+    
+    //Method to initialize Robot's pieces
+    public void Initialize(int type)
+    {
+        //Initialize piece type
+        switch (type)
         {
             case 0:
                 _type = pieceType.leg_l;
@@ -44,8 +52,80 @@ public class Piece : MonoBehaviour
                 _type = pieceType.head;
                 break;
         }
+
+        //Initialize conectors data
+        _numberConectors = Random.Range(0,_maxConectors+1);
+        _colors = new Color[_numberConectors];
+
+        //Instantiate the conectors
+        for (int i = 0; i < _numberConectors; i++)
+        {
+            Instantiate(_conectorPrefab, transform.GetChild(i + 1));        //The i+1 is because the first child in the prefab is the sprite.
+            _colors[i] = colors[Random.Range(0, colors.Length)];            //Pick random color.
+            if (transform.GetChild(i + 1).TryGetComponent<SpriteRenderer>(out SpriteRenderer spriteRenderer))
+            {
+                transform.GetChild(i + 1).gameObject.SetActive(true);
+                spriteRenderer.color = _colors[i];      //Asign color to component's spriteRenderer. 
+                                                        //(Must be a sprite. If not, pick the right component; not SpriteRenderer)
+            };
+        }
+
+        //Disable the Conectors we won't use. The prefab has 3 conectors, so we hide the ones we won't use.
+        for (int i = _numberConectors; i < _maxConectors; i++)
+        {
+            transform.GetChild(i + 1).gameObject.SetActive(false);
+        }
     }
 
+    //Method to initialize random pieces
+    public void Initialize()
+    {
+        //Initialize piece type
+        switch (Random.Range(0,5))
+        {
+            case 0:
+                _type = pieceType.leg_l;
+                break;
 
-    
+            case 1:
+                _type = pieceType.leg_r;
+                break;
+
+            case 2:
+                _type = pieceType.arm_l;
+                break;
+
+            case 3:
+                _type = pieceType.arm_r;
+                break;
+
+            case 4:
+                _type = pieceType.head;
+                break;
+        }
+
+        //Initialize conectors data
+        _numberConectors = Random.Range(0, _maxConectors + 1);
+        _colors = new Color[_numberConectors];
+
+        //Instantiate the conectors
+        for (int i = 0; i < _numberConectors; i++)
+        {
+            Instantiate(_conectorPrefab, transform.GetChild(i + 1));        //The i+1 is because the first child in the prefab is the sprite.
+            _colors[i] = colors[Random.Range(0, colors.Length)];            //Pick random color.
+            if (transform.GetChild(i + 1).TryGetComponent<SpriteRenderer>(out SpriteRenderer spriteRenderer))
+            {
+                transform.GetChild(i + 1).gameObject.SetActive(true);
+                spriteRenderer.color = _colors[i];      //Asign color to component's spriteRenderer. 
+                                                        //(Must be a sprite. If not, pick the right component; not SpriteRenderer)
+            };
+        }
+
+        //Disable the Conectors we won't use. The prefab has 3 conectors, so we hide the ones we won't use.
+        for (int i = _numberConectors; i < _maxConectors; i++)
+        {
+            transform.GetChild(i + 1).gameObject.SetActive(false);
+        }
+    }
+
 }
