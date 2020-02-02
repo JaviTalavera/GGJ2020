a03fafb1a8c422b4249133c7505b40f656b55921
AlvaroRoger <49962993+AlvaroRoger@users.noreply.github.com>
2020-02-02 04:08:49 +0100

    Menu

--- GGJ2020/Assets/Scripts/MenuOption.cs
+++ GGJ2020/Assets/Scripts/MenuOption.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuOption : MonoBehaviour
 {
     public GameObject joint;
+    public GameObject credits;
     public float force;
     public int id; //1. Jugar 2. Musica 3. Creditos 4. Salir
 
@@ -13,19 +15,25 @@ public class MenuOption : MonoBehaviour
         if(id==1 || id==4) joint.transform.position += Vector3.up * force * Time.deltaTime;
         if(id==2 || id==3) GetComponent<Rigidbody2D>().AddForce(Vector3.down * 50, ForceMode2D.Impulse);
         else GetComponent<Rigidbody2D>().AddForce(Vector3.down * 200, ForceMode2D.Impulse);
+
+        if(id==2)
+        {
+            //Musica ===> Julen, te toca a ti!
+        }
+
+        if (id ==3)
+        {
+            //Mostramos canvas
+            credits.SetActive(!credits.activeSelf);
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         switch (id){
             case 1:
-                //Load scene jugar
-                break;
-            case 2:
-                //Cambiar musica
-                break;
-            case 3:
-                //Mostrar créditos
+                SceneManager.LoadScene("1_Game");
+                print("Hola");
                 break;
             default:
                 Application.Quit();
