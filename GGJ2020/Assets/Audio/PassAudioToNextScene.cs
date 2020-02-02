4a5799b496b83ae24934235f088ad99ceedd021a
Julen <julenjus.creative@gmail.com>
2020-02-02 07:35:16 +0100

    Audio implemented in J_Test Scene.Just copy it when you want

---
+++ GGJ2020/Assets/Audio/PassAudioToNextScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassAudioToNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Play global
+    private static PassAudioToNextScene instance = null;
+    public static PassAudioToNextScene Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if(instance!= null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
